be775ee8c97130ac81c2aa6d2b80a477c62b4b65
dazao <zhaoxiaolong@dxy.cn>
2021-12-21 11:09:00 +0800

    feat: init ts project

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  sayGreeting();
+});
+
+const sayGreeting = (): void => {
+  setTimeout(() => {
+    let greeting: HTMLElement = document.createElement("p");
+    greeting.innerText = "I'm using TypeScript";
+    document.getElementById("info")?.appendChild(greeting);
+  }, 3000);
+};
