1f511b630b9c99fcb5ff3a0f64402f5f8819fc98
dazao <zhaoxiaolong@dxy.cn>
2021-12-27 15:13:08 +0800

    chore(2021-12-27): learn axios

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { AxiosRequestConfig } from './types'
+import xhr from './xhr'
+import { bulidURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/header'
+
+function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
+  xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+
+  return bulidURL(url, params)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+export default axios
